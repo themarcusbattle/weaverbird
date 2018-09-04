c0d557994b8d389e00581d31e670755f103e3294
Tammy Battle <tammybattle@airtammy.attlocal.net>
2018-09-03 22:42:54 -0400

    CPT & Taxonomy support

---
+++ cpts.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CampusBreak\Classes;
+
+class CPTs {
+
+    protected $name     = '';
+    protected $settings = [];
+
+    public function init() {
+        add_action( 'init', [ $this, 'register' ] );
+        add_action( 'save_post', [ $this, 'change_slug_to_ID' ] );
+        add_action( 'pre_get_posts', [ $this, 'add_post_types_to_main_query' ] );
+    }
+
+    /**
+     * Register the custom post type.
+     */
+    public static function register() {
+        register_post_type( $this->name, $this->settings );
+    }
+
+    /**
+     * Changes the post type slugs to IDs.
+     */
+    public function change_slug_to_ID( int $post_ID ) {
+
+        if ( wp_is_post_revision( $post_ID ) ) {
+            return;
+        }
+
+        // Unhook this function so it doesn't loop infinitely.
+		remove_action( 'save_post', [ $this, 'change_slug_to_ID' ] );
+
+		// Update the post, which calls save_post again.
+		wp_update_post( [
+            'ID'        => $post_ID,
+            'post_name' => $post_ID,
+        ] );
+
+		// Re-hook this function.
+		add_action( 'save_post', [ $this, 'change_slug_to_ID' ] );
+    }
+
+    /**
+     * Adds the custom post types to the main query.
+     */
+    public function add_post_types_to_main_query( \WP_Query $query ) {
+
+        if ( ! is_admin() && $query->is_main_query() && is_home() ) {
+
+            $custom_post_types = get_post_types( [
+                'public'   => true,
+                '_builtin' => false,
+            ] );
+
+            $query->set( 'post_type', $custom_post_types );
+        }
+
+        return $query;
+    }
+}
---
+++ taxonomies.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace CampusBreak\Classes;
+
+class Taxonomies {
+
+    protected $name     = '';
+    protected $objects  = [];
+    protected $settings = [];
+
+    public function init() {
+        add_action( 'init', [ $this, 'register' ] );
+        add_action( "{$this->name}_add_form_fields", [ $this, 'add_form_fields' ], 10 );
+        add_action( "{$this->name}_edit_form_fields", [ $this, 'edit_form_fields' ], 10 );
+        add_action( "create_{$this->name}", [ $this, 'save_form_fields_meta' ], 10 );
+        add_action( "edited_{$this->name}", [ $this, 'save_form_fields_meta' ], 10 );
+        add_filter( 'parent_file', [ $this, 'fix_taxonomy_page_for_users' ], 10 );
+    }
+
+    public static function register() {
+        register_taxonomy( $this->name, $this->objects, $this->settings );
+    }
+
+    public function add_form_fields( string $taxonomy ) {
+        return false;
+    }
+
+    public function edit_form_fields( \WP_Term $one ) {
+        return false;
+    }
+
+    public function save_form_fields_meta( int $term_ID ) {
+
+        $term_meta = $_POST['term_meta'] ?? false;
+
+        if ( ! $term_meta ) {
+            return;
+        }
+
+        foreach ( $term_meta as $meta_key => $meta_value ) {
+            update_term_meta( $term_ID, $meta_key, $meta_value );
+        }
+    }
+
+    public function fix_taxonomy_page_for_users( string $parent_file ) {
+
+        if ( ! in_array( 'user', $this->objects ) ) {
+            return $parent_file;
+        }
+        
+        global $pagenow;
+
+        $taxonomy  = $_GET['taxonomy'] ?? '';
+        $post_type = $_GET['post_type'] ?? false;
+        
+        if ( ! empty( $_GET[ 'taxonomy' ] ) && $_GET[ 'taxonomy' ] == 'college' && $pagenow == 'edit-tags.php' && ! $post_type ) {
+            $parent_file = 'users.php';
+        }
+
+        return $parent_file;
+    }
+}
